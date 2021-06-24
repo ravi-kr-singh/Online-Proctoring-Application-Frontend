15de6e559530682764d97e09a4014b62c2545b1b
Ravi Kumar Singh <imrk016@gmail.com>
2021-06-24 17:04:48 +0530

    Update test.js

--- js/test.js
+++ js/test.js
@@ -24,9 +24,12 @@ function submitTest() {
 		a: 1,
 		b: 2
 	};
-	
+	var string1 = JSON.stringify(payload);
+
+	var parsed = JSON.parse(string1);
+
 	var data = new FormData();
-	data.append( "json", JSON.stringify( payload ) );
+	data.append( "json", parsed);
 
 	fetch('https://nmnrna.pythonanywhere.com/submit', {
 	method: 'POST', // or 'PUT'
