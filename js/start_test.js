f6c7af73d3e27933ebb7289bfeea3af532289950
Ravi Kumar Singh <imrk016@gmail.com>
2021-06-25 12:23:06 +0530

    minor changes

--- js/start_test.js
+++ js/start_test.js
@@ -1,4 +1,21 @@
+function requestFullScreen(element) {
+  // Supports most browsers and their versions.
+  var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+  if (requestMethod) { // Native full screen.
+      requestMethod.call(element);
+  } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+      var wscript = new ActiveXObject("WScript.Shell");
+      if (wscript !== null) {
+          wscript.SendKeys("{F11}");
+      }
+  }
+}
+
+var elem = document.documentElement; // Make the body go full screen.
+
 function startGame() {
+    requestFullScreen(elem);
     window.location.href = 'test.html';
   
 }
